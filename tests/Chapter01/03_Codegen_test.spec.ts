134d88fc2ebec54e6387fe4023b5f9c2fb6eae33
sanjyotee <sanjyotee98@gmail.com>
2026-06-01 20:32:45 +0530

    Add tags to Playwright test cases

--- tests/Chapter01/03_Codegen_test.spec.ts
+++ tests/Chapter01/03_Codegen_test.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('Codegen test case', async ({ page }) => {
+test('Codegen test case', {tag: ['@PlaywrightWithJenkins']}, async ({ page }) => {
   await page.goto('https://www.youtube.com/');
   await page.getByRole('combobox', { name: 'Search' }).click();
   await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
   await page.getByRole('combobox', { name: 'Search' }).press('Enter');
   await page.goto('https://www.youtube.com/watch?v=788GvvcfwTY&list=PLUeDIlio4THEn2jQpsVpTuABKmUzFC53J');
   await expect(page.getByRole('link', { name: 'Playwright with TypeScript by' })).toBeVisible();
+});
+
+
+test('Test 2 will fail', {tag: ['@PlaywrightWithJenkins']}, async ({ page }) => {
+  await page.goto('https://www.youtube.com/@testerstalk');
+  expect(true).toBe(false);
+
 });
